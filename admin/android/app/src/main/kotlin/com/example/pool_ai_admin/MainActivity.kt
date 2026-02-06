a9d28b97c7eee9a2b70defba7d5242e0f6451f55
zengyang <xxxx@xxx.xxx>
2026-02-06 18:10:31 +0800

    refector: admin 使用element-admin.

--- admin/android/app/src/main/kotlin/com/example/pool_ai_admin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pool_ai_admin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- admin/lib/config/api_config.dart
+++
@@ -1,7 +0,0 @@
-class ApiConfig {
-  static const String baseUrl = 'http://localhost:8000';
-  static const String adminLogin = '/api/admin/login';
-  static const String adminUsers = '/api/admin/users';
-  static const String apiKeys = '/api/admin/api-keys';
-  static const String posts = '/api/admin/posts';
-}
